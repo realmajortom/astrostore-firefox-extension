f2453398043193186ca2b109df800ea074fab9ff
Thomas Girgenti <thomasg@disroot.org>
2019-08-20 16:37:37 -0600

    Initial Commit

---
+++ precache-manifest.1575bab57ee4d6ad480bb654058ad1b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7596ab27833265e5f6eb58151f2cdbe7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc3fbc94dff29d0de54d",
+    "url": "/static/css/main.731f8a1b.chunk.css"
+  },
+  {
+    "revision": "03897efa9f2efe26fc8a",
+    "url": "/static/js/2.9875a667.chunk.js"
+  },
+  {
+    "revision": "cc3fbc94dff29d0de54d",
+    "url": "/static/js/main.5e3c863c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
