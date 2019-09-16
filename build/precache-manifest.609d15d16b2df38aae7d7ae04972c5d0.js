d6693bb6e98eaf2a26e8581335a446a5cad5ac1e
Thomas Girgenti <thomasg@disroot.org>
2019-09-16 12:01:02 -0600

    fix body width

--- build/precache-manifest.609d15d16b2df38aae7d7ae04972c5d0.js
+++ build/precache-manifest.609d15d16b2df38aae7d7ae04972c5d0.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7596ab27833265e5f6eb58151f2cdbe7",
+    "revision": "84572cd8e3d2b20134a71558f25aa452",
     "url": "/index.html"
   },
   {
-    "revision": "cc3fbc94dff29d0de54d",
-    "url": "/static/css/main.731f8a1b.chunk.css"
+    "revision": "34fad137f460fc44df11",
+    "url": "/static/css/main.16416a9f.chunk.css"
   },
   {
     "revision": "03897efa9f2efe26fc8a",
     "url": "/static/js/2.9875a667.chunk.js"
   },
   {
-    "revision": "cc3fbc94dff29d0de54d",
-    "url": "/static/js/main.5e3c863c.chunk.js"
+    "revision": "34fad137f460fc44df11",
+    "url": "/static/js/main.75027b37.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1575bab57ee4d6ad480bb654058ad1b1.js"
+  "/precache-manifest.609d15d16b2df38aae7d7ae04972c5d0.js"
 );
 
 self.addEventListener('message', (event) => {
